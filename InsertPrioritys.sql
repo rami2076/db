6c9d1ff3b689a949062300c8e5a95b9f3e959355
rami2076 <sakusennx@yahoo.co.jp>
2016-10-27 18:01:26 +0900

    add status green and blue.

--- InsertPrioritys.sql
+++ InsertPrioritys.sql
@@ -3,5 +3,7 @@ checksum table prioritys;
 Truncate table prioritys;
 checksum table prioritys;
 Insert Into  prioritys(prooritys) values('red');
+Insert Into  prioritys(prooritys) values('blue');
+Insert Into  prioritys(prooritys) values('green');
 select * from prioritys;
 checksum table prioritys;
