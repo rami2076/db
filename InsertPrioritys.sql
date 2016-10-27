188bb5f437819c776a42a5c167b44d42ebbf8117
rami2076 <sakusennx@yahoo.co.jp>
2016-10-27 17:54:27 +0900

    Update Insert script for reset autoIncrement value.

--- InsertPrioritys.sql
+++ InsertPrioritys.sql
@@ -1,1 +1,7 @@
+select * from prioritys;
+checksum table prioritys;
+Truncate table prioritys;
+checksum table prioritys;
 Insert Into  prioritys(prooritys) values('red');
+select * from prioritys;
+checksum table prioritys;
